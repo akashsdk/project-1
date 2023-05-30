68f5609791603007f9cf30714e098d8fe6043455
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-05-30 23:49:14 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from'./Screen/home.js';
+import Header from './Component/header';
+import Index from "./Screen/index";
+import Footer from './Component/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Home />
+      <Index />
+      <Footer />
     </div>
   );
 }
---
+++ src/Screen/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../Style/home.css";
+
+export default function home() {
+  return (
+    <div className='homeBody'>home
+    </div>
+    
+  )
+}
