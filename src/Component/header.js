bcca90af3af35614f4347fa92e30139bbc142a3d
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-05-31 23:32:34 +0600

    ok

--- src/Component/header.js
+++ src/Component/header.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="headerBody">
-      <h2>Project-1</h2>
-      <div>
+      <h2 className="headerTitle">Project-1</h2>
+      <div className="headerBox">
         <Link className="headerLink" to="/">Home</Link>
         <Link className="headerLink" to="/About">About</Link>
         <Link className="headerLink" to="/ContactUs">Contact Us</Link>
